e172655103a75795c548045aec1dcfd0d2da01ae
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-06-11 18:00:31 +0700

    menambahkan metode insert untuk menambahkan node ke dalam binary search tree

--- searchTree.cpp
+++ searchTree.cpp
@@ -32,7 +32,22 @@ public:
    // Insert a Node in the binary search tree
    void insert(string element)
    {
+                Node *newNode = new Node(element, NULL, NULL);
 
+        newNode->info = element;
+        newNode->leftchild = NULL;
+        newNode->rightchild = NULL;
+
+        Node *parent = NULL;
+        Node *currentNode = NULL;
+
+        search(element, parent, currentNode);
+
+        if (parent == NULL) // Tree is empty
+        {
+            ROOT = newNode;
+            return;
+        }
    }
 
 };
